12139e3550e23b43df153b2b87449f964f596452
Simon Jones <github@simonjjones.com>
2021-04-16 11:07:10 -0400

    directory structure moving around

---
+++ backend/lib/helpers.ex
@@ -0,0 +1,8 @@
+defmodule Helpers do
+  def getApiPrefix() do
+    "http://api.simonjjones.com/"
+  end
+  def sendRequest(url, jsonBody) do
+    # FIXME: do stuff
+  end
+end
---
+++ rust.nix
@@ -0,0 +1,18 @@
+{ pkgs ? import <unstable> {} }:
+
+let
+  shellname = "www";
+  myRust = with pkgs; [ elixir elm ];
+in
+  pkgs.stdenv.mkDerivation {
+    name = shellname;
+    buildInputs = [ myRust ];
+    RUST_BACKTRACE = 1;
+    shellHook = ''
+      export NIX_SHELL_NAME='${shellname}'
+      alias mb='mix build'
+      alias mf='mix fmt'
+      alias mr='mix run'
+      alias mt='mix test'
+    '';
+  }
