25a0ab57c3305214419aa35be365beee13d3d0f1
Simon Jones <github@simonjjones.com>
2021-04-16 11:34:24 -0400

    messing

--- frontend/src/Main.elm
+++ frontend/src/Main.elm
@@ -1,3 +1,4 @@
+module Main exposing (..)
 import Browser
 import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
@@ -5,19 +6,31 @@ import Html.Events exposing (onClick)
 main =
   Browser.sandbox { init = 0, update = update, view = view }
 
-type Msg = Increment | Decrement
+type Msg = Inc | Dec | BigInc | BigDec | Squared | Zero
 
 update msg model =
   case msg of
-    Increment ->
+    Inc ->
       model + 1
-
-    Decrement ->
+    BigInc ->
+      model + 5
+    Dec ->
       model - 1
+    BigDec ->
+      model - 5
+    Squared ->
+      model * model
+    Zero ->
+      0
 
 view model =
   div []
-    [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (String.fromInt model) ]
-    , button [ onClick Increment ] [ text "+" ]
+    [
+      div [] [ button [ onClick Inc ] [ text "+" ] ],
+      div [] [ button [ onClick BigInc ] [ text "++" ] ],
+      div [] [ button [ onClick BigDec ] [ text "--" ] ],
+      div [] [ button [ onClick Dec ] [ text "-" ] ],
+      div [] [ button [ onClick Squared ] [ text "^2" ] ],
+      div [] [ button [ onClick Zero ] [ text "0" ] ],
+      div [] [ text (String.fromInt model) ]
     ]
